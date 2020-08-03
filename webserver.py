0ecbfc41e8c14354811bcc6043a625281187ef2b
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-03 11:12:37 -0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,52 +1,31 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required
+from flask import Flask
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
 
 # Create app
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super-secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-
-# Create database connection object
-db = SQLAlchemy(app)
-
-# Define models
-roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
-
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
 
 # Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
 security = Security(app, user_datastore)
 
 # Create a user to test with
 @app.before_first_request
 def create_user():
-    db.create_all()
+    init_db()
     user_datastore.create_user(email='matt@nobien.net', password='password')
-    db.session.commit()
+    db_session.commit()
 
 # Views
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    return render('Here you go!')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port='8082')
+    app.run(host='0.0.0.0',port='8081')
